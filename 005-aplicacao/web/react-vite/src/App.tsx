72269767891b44d2240c8b73b7585f97844e389c
Lincoln Rebouças <lincoln.reboucas40@gmail.com>
2024-01-16 20:26:09 -0300

    code aula 13 - web

---
+++ 005-aplicacao/web/react-vite/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Sistema de Doação de Sangue</h1>
+
+      <h2>Elementos: {count} </h2>
+
+      <button onClick={() => setCount(count + 1)}>Adicionar</button>
+      <button onClick={() => setCount(count - 1)}>Remover</button>
+      <button onClick={() => setCount(count * 2)}>Dobrar</button>
+
+
+    </>
+  )
+}
+
+export default App
